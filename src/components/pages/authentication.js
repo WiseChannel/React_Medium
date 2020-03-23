1e7499be19fdd4d0886b358b1f411ae0bf824358
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-03-23 12:40:29 +0200

    [add] worked on the registration page. Fix 422 status reg

--- src/components/pages/authentication.js
+++ src/components/pages/authentication.js
@@ -5,58 +5,77 @@ import { Link } from "react-router-dom";
 import axios from 'axios'
 import useFetch from "../Hooks/useFetch";
 
-const Authentication = () => {
+const Authentication = props => {;
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [{isLoading, error, response}, doFetch] = useFetch('/users/login')
+    const isLogin = props.match.path === '/login';
+    const pageTitle = isLogin ? 'Sign In' : 'Sign Up';
+    const descriptionLink = isLogin ? '/register' : '/login';
+    const descriptionText = isLogin ? 'Need an accoutn' : 'Have an account?';
+    const apiUrl = isLogin ? '/user/login' : '/users'
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userName, setUserName] = useState('')
+    const [{isLoading, error, response}, doFetch] = useFetch(apiUrl);
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const user = isLogin ? {email, password} : {email, password, userName}
 
         doFetch({
             method: 'post',
             data: {
-                user: {
-                    email: 'nameUser@mail.com',
-                    password: '2021'
-                }
+                user
             }
         })
-    }
+    };
 
     return (
-        <div className='auth-page'>
+        <div className="auth-page">
             <div className="container page">
                 <div className="row">
                     <div className="col-md-6 offset-md-3 col-xs-12">
-                        <h1 className='text-xs-center'>Login</h1>
-                        <p className='text-xs-center'>
-                            <Link to='register'>Need an account?</Link>
+                        <h1 className="text-xs-center">{pageTitle}</h1>
+                        <p className="text-xs-center">
+                            <Link to={descriptionLink}>{descriptionText}</Link>
                         </p>
                         <form onSubmit={handleSubmit}>
                             <fieldset>
-                                <fieldset className='form-group'>
+                                {!isLogin && (
+                                    <fieldset className="form-group">
+                                        <input
+                                            type="text"
+                                            className="form-control form-control-lg"
+                                            placeholder="Username"
+                                            value={userName}
+                                            onChange={e => setUserName(e.target.value)}
+                                        />
+                                    </fieldset>
+                                )}
+                                <fieldset className="form-group">
                                     <input
-                                        type='email'
-                                        className='form-control from-control-light'
-                                        placeholder='Email'
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="Email"
                                         value={email}
                                         onChange={e => setEmail(e.target.value)}
                                     />
                                 </fieldset>
-
-                                <fieldset className='form-group'>
+                                <fieldset className="form-group">
                                     <input
-                                        type='password'
-                                        className='form-control from-control-light'
-                                        placeholder='Password'
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Password"
                                         value={password}
                                         onChange={e => setPassword(e.target.value)}
                                     />
                                 </fieldset>
-                                <button className='btn btn-lg btn-primary pull-xs-right' type='submit' disabled={isLoading}>
-                                    Sign in
+                                <button
+                                    disabled={isLoading}
+                                    className="btn btn-lg btn-primary pull-xs-right"
+                                    type="submit"
+                                >
+                                    {pageTitle}
                                 </button>
                             </fieldset>
                         </form>
