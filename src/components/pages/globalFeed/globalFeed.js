0c0b8b1d464a188660f8aff24d895acff024bd0d
Ilya <ilyaphotoshop17@gmail.com>
2020-03-28 22:21:56 +0200

    [add] created Fedd components

--- src/components/pages/globalFeed/globalFeed.js
+++ src/components/pages/globalFeed/globalFeed.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, Fragment } from 'react'
+
+import useFetch from '../../Hooks/useFetch'
+import Feed from '../../feed'
 
 const GlobalFeed = () => {
-    return <div>GlobalFeed</div>
+
+    const apiUrl = '/articles?limit=10&offset=0'
+    const [{response, error, isLoading}, doFetch] = useFetch(apiUrl)
+    console.log('Response from globalFeed: ', response);
+    
+    useEffect(() => {
+        doFetch()
+    }, [doFetch])
+
+    return (
+        <div className='home-page'>
+            <div className='banner'>
+                <div className='container'>
+                    <h1>Medium clone</h1>
+                    <p>A place to share</p>
+                </div>
+            </div>
+            <div className='container page'>
+                <div className='row'>
+                <div className="col-md-9">
+                    {isLoading && <div>Loading...</div>}
+                    {error && <div>Some error happened</div>}
+                    {!isLoading && response && (
+                    <Fragment>
+                        <Feed articles={response.articles} />
+                    </Fragment>
+                    )}
+                 </div>
+                    <div className='col-md-3'>
+                        Popular tags 
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
 }
 
 export default GlobalFeed
