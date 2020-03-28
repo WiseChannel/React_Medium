7848c907a8e4f4d88c3d7f267b91be6c76246fd1
Ilya <ilyaphotoshop17@gmail.com>
2020-03-28 22:49:27 +0200

    [fix] feed className

--- src/components/feed.js
+++ src/components/feed.js
@@ -6,7 +6,7 @@ const Feed = ({articles}) => {
     return (
         <div>
              {articles.map((article, index) => (
-            <div className='article-previev' key={index}>
+            <div className='article-preview' key={index}>
                 <div className='article-meta'>
                  <Link to={`/profiles/${article.author.username}`}>
                      <img src={article.author.image} alt="" />
