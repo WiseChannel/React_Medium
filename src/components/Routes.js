0ac9d50ddcc3a58c3f8472b6e72b65e52fd98b3e
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-03-29 19:04:14 +0300

    [add] create tour feed component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -6,11 +6,13 @@ import GlobalFeed from './pages/globalFeed/globalFeed'
 import Article from './pages/article/article'
 import Authentication from "./pages/authentication";
 import TagFeed from "./pages/tagFeed/tagFeed";
+import YourFeed from "./pages/tagFeed/tagFeed";
 
 export default () => {
     return (
         <Switch>
             <Route exact path='/' component={GlobalFeed} />
+            <Route path='/feed' component={YourFeed} />
             <Route exact path='/tags/:slug' component={TagFeed} />
             <Route path='/login' component={Authentication} />
             <Route path='/register' component={Authentication} />
---
+++ src/components/pages/YourFeed/YourFeed.js
@@ -0,0 +1,60 @@
+import React, { useEffect, Fragment } from 'react'
+import {stringify} from "query-string";
+
+import useFetch from '../../Hooks/useFetch'
+import Feed from '../components/feed'
+import Pagination from "../../pagination/pagination";
+import { getPaginator, limit } from "../../pagination/utils";
+import PopularTags from "../../popularTags/popularTags";
+import Loading from "../components/loading";
+import ErrorMessage from "../components/errorMessage";
+import FeedTogler from "../components/feedTogler";
+
+const YourFeed = ({location, match}) => {
+
+    const {offset, currentPage} = getPaginator(location.search)
+    console.log('off, currP: ', currentPage, offset)
+    const stringifiedParams = stringify({
+         limit,
+         offset
+    })
+    const url = match.url
+    const apiUrl = `/articles/feed?${stringifiedParams}`
+    const [{response, error, isLoading}, doFetch] = useFetch(apiUrl)
+    console.log('Response from globalFeed: ', response);
+
+    useEffect(() => {
+        doFetch()
+    }, [doFetch, currentPage])
+
+    return (
+        <div className='home-page'>
+            <div className='banner'>
+                <div className='container'>
+                    <h1>Medium clone</h1>
+                    <p>A place to share</p>
+                </div>
+            </div>
+            <div className='container page'>
+                <div className='row'>
+                <div className="col-md-9">
+                    <FeedTogler/>
+                    {isLoading && <Loading/>}
+                    {error && <ErrorMessage/>}
+                    {!isLoading && response && (
+                        <Fragment>
+                            <Feed articles={response.articles} />
+                            <Pagination total={response.articlesCount} limit={limit} url={url} currentPage={currentPage} />
+                        </Fragment>
+                        )}
+                 </div>
+                    <div className='col-md-3'>
+                        <PopularTags/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+}
+
+export default YourFeed
