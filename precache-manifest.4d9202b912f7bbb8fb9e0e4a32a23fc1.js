347fd39ff20b3fea645699d34aeea854d061d054
Ilya <ilyaphotoshop17@gmail.com>
2020-04-14 02:11:26 +0300

    Updates

---
+++ precache-manifest.4d9202b912f7bbb8fb9e0e4a32a23fc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d8050674d9f984628e82e64816c154",
+    "url": "/React_Medium/index.html"
+  },
+  {
+    "revision": "ca2621887d5b67d6aadf",
+    "url": "/React_Medium/static/js/2.91d87ec1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Medium/static/js/2.91d87ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f362dccf8d680d752768",
+    "url": "/React_Medium/static/js/main.eebe8487.chunk.js"
+  },
+  {
+    "revision": "9ee0977281ccd6e2496b",
+    "url": "/React_Medium/static/js/runtime-main.d1d1897b.js"
+  }
+]);
